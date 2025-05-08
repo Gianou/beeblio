4d473bb697a404ea3578c09848c44b03e701d167
Gianou <david.gianadda@students.hevs.ch>
2025-05-08 23:05:20 +0200

    Remove user greetings logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,10 @@ import "./App.css";
 
 function App() {
   const appName = "Beeblio";
-  const date = new Date();
-
-  const greetsAccodringToPeriodOfDay = () => {
-    let periodOfDay = date.getHours() < 12 ? "morning" : "afternoon";
-    return <p>Good {periodOfDay}!</p>;
-  };
 
   return (
     <div>
       <h1>Welcome to {appName}!</h1>
-      <p>
-        The date is currently : {date.toLocaleDateString()}{" "}
-        {date.toLocaleTimeString()}
-      </p>
-      {greetsAccodringToPeriodOfDay()}
-      <p>
-        <a
-          href="https://en.wikipedia.org/wiki/Bee"
-          target="_blank"
-          rel="noreferrer"
-          className="App-link"
-        >
-          Learn about bees
-        </a>
-      </p>
-      <p>
-        <a
-          href="https://en.wikipedia.org/wiki/Library"
-          target="_blank"
-          rel="noreferrer"
-          className="App-link"
-        >
-          Learn about libraries
-        </a>
-      </p>
     </div>
   );
 }
