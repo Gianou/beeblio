f3a58c2335cdfdc58cd97139953f01b7fb829d59
Gianou <david.gianadda@students.hevs.ch>
2025-05-08 23:04:27 +0200

    Restore the use of App as the root component & move the previous JSX from main to App

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// The App component will be used in the future exercices
 import App from "./App.jsx";
 
-const appName = "Beeblio";
-const date = new Date();
-
-const greetsAccodringToPeriodOfDay = () => {
-  let periodOfDay = date.getHours() < 12 ? "morning" : "afternoon";
-  return <p>Good {periodOfDay}!</p>;
-};
-
 createRoot(document.getElementById("root")).render(
-  <div>
-    <h1>Welcome to {appName}!</h1>
-    <p>
-      The date is currently : {date.toLocaleDateString()}{" "}
-      {date.toLocaleTimeString()}
-    </p>
-    {greetsAccodringToPeriodOfDay()}
-    <p>
-      <a
-        href="https://en.wikipedia.org/wiki/Bee"
-        target="_blank"
-        rel="noreferrer"
-        className="App-link"
-      >
-        Learn about bees
-      </a>
-    </p>
-    <p>
-      <a
-        href="https://en.wikipedia.org/wiki/Library"
-        target="_blank"
-        rel="noreferrer"
-        className="App-link"
-      >
-        Learn about libraries
-      </a>
-    </p>
-  </div>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
