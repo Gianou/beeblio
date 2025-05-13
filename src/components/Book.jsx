929594c364cbd8e211bbf050ee1e7052c6d24936
Gianou <david.gianadda@students.hevs.ch>
2025-05-13 16:46:12 +0200

    Added like button to Book with useState

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 function Book({ title, author, year, pages, cover }) {
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
     <div>
       <h2>{title}</h2>
       <h4>{author}</h4>
       <p>Published in : {year}</p>
       <p>Pages : {pages}</p>
+
+      <p>
+        <button
+          onClick={() => {
+            setIsLiked((previousIsLiked) => !previousIsLiked);
+          }}
+        >
+          Like: {isLiked.toString()}
+        </button>
+      </p>
+
       <img alt={title} src={cover} style={{ maxHeight: "300px" }} />
     </div>
   );
