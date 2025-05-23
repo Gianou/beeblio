1273b55c630a98ef6c1cffd8ba68725e910a1810
Gianou <david.gianadda@students.hevs.ch>
2025-05-23 09:38:45 +0200

    Removed comments from previous exercise

--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,34 +1,24 @@
 import { useState } from "react";
 
 function AddBookForm({ books, setBooks }) {
-  // Empty book template for resetting the form
   const emptyBook = { title: "", author: "", year: "", pages: "", cover: "" };
 
-  // State for the new book -> stores the data about the new book we are preparing in the form
   const [newBook, setNewBook] = useState(emptyBook);
 
   const handleFormInputChange = (event) => {
-    // The syntax below is called object destructuring, it achieves the same as the commented code
-    // const name = event.target.name;
-    // const value = event.target.value;
     const { name, value } = event.target;
-    console.log(event.target);
-
     setNewBook((prevNewBook) => ({ ...prevNewBook, [name]: value })); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
   };
 
-  // Form submit handler
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    //console.log(newBook);
     setBooks([newBook, ...books]);
-    setNewBook(emptyBook); // Reset the form
+    setNewBook(emptyBook);
   };
 
   return (
     <div>
       <h2>Add a new Book</h2>
-      {/* The css is defined in App.css */}
       <form onSubmit={handleFormSubmit} className="NewBook-Form">
         <input
           type="text"
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 
-// Note that the Book component was refactored to accept a book object instead of each prop separately
 function Book({ bookData }) {
   const [isLiked, setIsLiked] = useState(
     localStorage.getItem(bookData.title)
@@ -13,7 +12,6 @@ function Book({ bookData }) {
     localStorage.setItem(bookData.title, JSON.stringify(isLiked));
   }, [isLiked]);
 
-  /* 2. Element variable for the formatted book title */
   let formattedTitle;
   if (isLiked) {
     formattedTitle = <h2 style={{ color: "lightgreen" }}>{bookData.title}</h2>;
@@ -23,7 +21,6 @@ function Book({ bookData }) {
 
   return (
     <div>
-      {/* Render the formatted title element variable */}
       {formattedTitle}
       <h4>{bookData.author}</h4>
       <p>Published in : {bookData.year}</p>
@@ -35,7 +32,6 @@ function Book({ bookData }) {
             setIsLiked((previousIsLiked) => !previousIsLiked);
           }}
         >
-          {/* 1. Conditional rendering with inline ternary operator */}
           {isLiked ? "❤️" : "🤍"}
         </button>
       </p>
